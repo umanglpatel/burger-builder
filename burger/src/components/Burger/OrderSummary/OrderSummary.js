dd4effbd68667ee016b45a52b015903fa43b714b
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-08-13 16:07:54 +0530

    Section 8.19 creating the order summary modal

---
+++ burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import HocAux from '../../../hoc/HocAux';
+
+const orderSumamry = (props) => {
+    const ingredientSummary = Object.keys(props.ingredients)
+        .map(igKey => {
+            return (
+                <li key={igKey}>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey}</span> : {props.ingredients[igKey]}
+                </li>);
+        });
+    return (
+        <HocAux>
+            <h3>Your Order</h3>
+            <p>A delidious burger with the following ingredients:</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p>Continue to Checkout?</p>
+        </HocAux>
+    );
+};
+
+export default orderSumamry;
