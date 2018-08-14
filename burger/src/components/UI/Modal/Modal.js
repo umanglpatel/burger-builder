87fda30160baae39f7a8e56b29d11bbc60e14b73
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-08-14 15:37:29 +0530

    Section 8.21 Implementing the backdrop component and styling. So user can click on background to cancel order popup.

--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -1,14 +1,20 @@
 import React from 'react';
 
 import classes from './Modal.css';
+import HocAux from '../../../hoc/HocAux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const modal = (props) => (
-    <div
-        className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}> {props.children}</div>
+    <HocAux>
+        <Backdrop show={props.show} clicked={props.modalClosed} />
+        <div
+            className={classes.Modal}
+            style={{
+                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity: props.show ? '1' : '0'
+            }}>
+            {props.children}</div>
+    </HocAux>
 );
 
 export default modal;
--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -74,6 +74,10 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
     render() {
         const disabledInfo = { ...this.state.ingredients };
         for (let key in disabledInfo) {
@@ -82,7 +86,7 @@ class BurgerBuilder extends Component {
 
         return (
             <HocAux>
-                <Modal show={this.state.purchasing}>
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
                     <OrderSummary ingredients={this.state.ingredients} />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
