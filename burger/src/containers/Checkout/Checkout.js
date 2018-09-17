b62c8b70c2ec291013e122697007c155a3d61175
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-09-17 17:33:37 +0530

    Section 12.7 Navigating to contact-data-component.

--- burger/src/containers/Checkout/Checkout.js
+++ burger/src/containers/Checkout/Checkout.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
 
@@ -37,6 +39,8 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelled}
                     checkoutContinued={this.checkoutContinued} />
+                <Route path={this.props.match.path + '/contact-data'}
+                    component={ContactData} />
             </div>
         );
     }
