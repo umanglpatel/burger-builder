03e72d1851fb0c986008c16bbac7094145dc9a08
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-10-26 11:29:39 +0530

    Section 13.11 Adding validation feedback.

--- burger/src/components/UI/Input/Input.js
+++ burger/src/components/UI/Input/Input.js
@@ -5,11 +5,15 @@ import classes from './Input.css';
 const input = (props) => {
 
     let inputElement = null;
+    const inputClasses = [classes.InputElement];
+    if (props.invalid && props.shouldValidate) {
+        inputClasses.push(classes.Invalid);
+    }
 
     switch (props.elementType) {
         case ('input'):
             inputElement = <input
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 {...props.elementConfig}
                 value={props.value}
                 onChange={props.changed} />;
@@ -22,7 +26,7 @@ const input = (props) => {
             break;
         case ('select'):
             inputElement = (<select
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 value={props.value}
                 onChange={props.changed}>
                 {props.elementConfig.options.map(option => (
