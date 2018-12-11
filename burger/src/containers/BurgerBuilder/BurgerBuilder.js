f2e8f717ab4740315ba22bfe9eed0b2272d32560
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-12-11 16:41:05 +0530

    Section 15.4 ingredient reducer.

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,13 +33,13 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
-        axios.get('https://react-my-burger-f82ba.firebaseio.com/ingredients.json')
-            .then(response => {
-                this.setState({ ingredients: response.data });
-            })
-            .catch(error => {
-                this.setState({ error: true })
-            });
+        // axios.get('https://react-my-burger-f82ba.firebaseio.com/ingredients.json')
+        //     .then(response => {
+        //         this.setState({ ingredients: response.data });
+        //     })
+        //     .catch(error => {
+        //         this.setState({ error: true })
+        //     });
     }
 
     updatePurchaseState(ingredients) {
