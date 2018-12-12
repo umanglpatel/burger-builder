869679f27680ea8072ef07878f082142dc9336db
Umang Patel <umang.patel@AMD.IBASET.COM>
2018-12-12 11:19:48 +0530

    Section 15.7 redux and ui state order now button

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -19,7 +19,6 @@ class BurgerBuilder extends Component {
     // }
 
     state = {
-        purchasable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -42,8 +41,8 @@ class BurgerBuilder extends Component {
             })
             .reduce((sum, el) => {
                 return sum + el;
-            }, 0)
-        this.setState({ purchasable: sum > 0 });
+            }, 0);
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -86,7 +85,7 @@ class BurgerBuilder extends Component {
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabledInfo={disabledInfo}
                         price={this.props.price}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler} />
                 </HocAux>
             );
