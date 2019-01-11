053f0285618892127b77e8fe841b2a1612b11e0a
Umang Patel <umang.patel@AMD.IBASET.COM>
2019-01-11 15:20:42 +0530

    Section 17.7 Initializing ingredients.

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        this.props.onInitIngredients();
     }
 
     updatePurchaseState(ingredients) {
@@ -55,7 +56,7 @@ class BurgerBuilder extends Component {
         }
         let orderSummary = null;
 
-        let burger = this.state.error ? <p>Ingredients can't be laoded</p> : <Spinner />
+        let burger = this.props.error ? <p>Ingredients can't be laoded</p> : <Spinner />
         if (this.props.ings) {
             burger = (
                 <HocAux>
@@ -92,14 +93,16 @@ class BurgerBuilder extends Component {
 const mapStateToProps = (state) => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        error: state.error
     };
 }
 
 const mapDispatchToProps = (dispatch) => {
     return {
         onIngredientAdded: (ingName) => dispatch(burgerBuilderActions.addIngredient(ingName)),
-        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName))
+        onIngredientRemoved: (ingName) => dispatch(burgerBuilderActions.removeIngredient(ingName)),
+        onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients())
     };
 }
 
