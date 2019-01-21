b535ca82da5aef1af59e5698746b01b249b598d8
Umang Patel <umang.patel@AMD.IBASET.COM>
2019-01-21 16:46:25 +0530

    Section 18.10 : logout

--- burger/src/store/actions/auth.js
+++ burger/src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeOut = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    }
+}
+
 export const auth = (email, password, isSignUp) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,6 +52,7 @@ export const auth = (email, password, isSignUp) => {
             .then(response => {
                 console.log(response);
                 dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeOut(response.data.expiresIn));
             })
             .catch(err => {
                 dispatch(authFail(err.response.data.error));
