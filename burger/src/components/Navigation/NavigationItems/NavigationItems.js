9a21fb8a83acb093b54f1efe1d1a00b46565557a
Umang Patel <umang.patel@AMD.IBASET.COM>
2019-01-24 15:11:14 +0530

    Section 18.12 updating the UI depending on auth state.

--- burger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -8,7 +8,9 @@ const navigationItems = (props) => (
         {/* for boolean active value we can do "active=true" or simply only "active" */}
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
         <NavigationItem link="/orders">Orders</NavigationItem>
-        <NavigationItem link="/auth">Authenticate</NavigationItem>
+        {props.isAuthenticated
+            ? <NavigationItem link="/logout">Logout</NavigationItem>
+            : <NavigationItem link="/auth">Authenticate</NavigationItem>}
     </ul>
 );
 
--- burger/src/hoc/Layout/Layout.js
+++ burger/src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import HocAux from '../HocAux/HocAux';
 import classes from './Layout.css';
@@ -24,8 +25,11 @@ class Layout extends Component {
     render() {
         return (
             <HocAux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
+                <Toolbar
+                    isAuth={this.props.isAuthenticated}
+                    drawerToggleClicked={this.sideDrawerToggleHandler} />
                 <SideDrawer
+                    isAuth={this.props.isAuthenticated}
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
@@ -36,5 +40,10 @@ class Layout extends Component {
     }
 }
 
+const mapStateToProps = (state) => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
 
-export default Layout;
+export default connect(mapStateToProps)(Layout);
